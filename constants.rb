7448744907b4b38a3970f3fef25fe992cdb6d01b
GED <gimaev.ufa@gmail.com>
2023-08-07 12:32:05 +0500

    Returned all ranks

--- constants.rb
+++ constants.rb
@@ -6,11 +6,11 @@ module Constants
   MAX_SCORE = 21
 
   SUITS = ['♠', '♥', '♣', '♦'].freeze
-  RANKS = %w[A 2 ].freeze
+  RANKS = %w[A 2 3 4 5 6 7 8 9 T J Q K ].freeze
   VALUES = {
     'A' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9,
     'T' => 10, 'J' => 10, 'Q' => 10, 'K' => 10
   }.freeze
 end
 
-#3 4 5 6 7 8 9 T J Q K
+#
